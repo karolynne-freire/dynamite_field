7fb54ace4765ab66c3f124487f538f00209c06ea
karolynne-freire <freirekarolynne@gmail.com>
2025-07-04 12:32:08 -0300

    feat: incluindo os menus

---
+++ back/fluxo.py
@@ -0,0 +1,17 @@
+def menu_principal():
+    print('--- Jogo Campo Minado ---')
+    print('(1) Começar o jogo')
+    print('(2) Recomeçar o último jogo')
+    print('(3) Os cinco melhores tempos')
+    print('(4) Sair')
+    
+    opcao = input('Escolha uma das opções acima: ')
+    return opcao
+
+def menu_dificuldade ():
+    print('\n(1) Fácil')
+    print('(2) Médio')
+    print('(3) Voltar')
+   
+    opcao = input('Escolha uma das opções acima: ')
+    return opcao
